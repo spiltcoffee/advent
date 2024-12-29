a4bd19e18e7a2ba2902e838bcd60a81aa828e19c
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-30 02:33:31 +1100

    Add 2021 day 1 part 2

--- 2021/1/answer.ts
+++ 2021/1/answer.ts
@@ -1,15 +1,33 @@
 import { AnswerFunction } from "../../answer.ts";
 
+function sum(nums: number[]): number {
+  return nums.reduce((total, num) => total + num, 0);
+}
+
 export const answer: AnswerFunction = ([input]) => {
-  const depths = input.split("\n").map(str => Number.parseInt(str, 10));
-  
-  let increments = 0;
+  const depths = input.split("\n").map((str) => Number.parseInt(str, 10));
+
+  let part1Increments = 0;
   depths.reduce((prev, curr) => {
     if (curr > prev) {
-      increments++;
+      part1Increments++;
     }
     return curr;
   }, Infinity);
 
-  return [increments.toString(), ""];
+  let part2Increments = 0;
+  depths.reduce(
+    (prevArr, curr) => {
+      const currArr = prevArr.slice(1).concat(curr);
+
+      if (sum(currArr) > sum(prevArr)) {
+        part2Increments++;
+      }
+
+      return currArr;
+    },
+    [Infinity, Infinity, Infinity]
+  );
+
+  return [part1Increments.toString(), part2Increments.toString()];
 };
