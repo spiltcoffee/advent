a75ae79b4b8b3fedd9dff4c1844fab9a86419056
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-30 21:20:40 +1100

    Add 2020 day 1 part 2

--- 2020/1/answer.ts
+++ 2020/1/answer.ts
@@ -1,15 +1,39 @@
 import { AnswerFunction } from "../../answer.ts";
 import { Maths } from "../../common/maths.ts";
 
-export const answer: AnswerFunction = ([input]) => {
-  const expenses = input.split("\n").map(Maths.parseInt);
-
+function findTwoExpensesProduct(expenses: number[]): number {
   const firstEntry = expenses.find((number, index, array) => {
     const inverse = 2020 - number;
     return array.slice(index + 1).includes(inverse);
   });
 
-  const entryProduct = firstEntry * (2020 - firstEntry);
+  return firstEntry * (2020 - firstEntry);
+}
 
-  return [entryProduct];
+function findThreeExpensesProduct(expenses: number[]): number {
+  let index: number;
+  let firstEntry: number;
+  let secondEntry: number;
+  for ([index, firstEntry] of expenses.entries()) {
+    const firstInverse = 2020 - firstEntry;
+    secondEntry = expenses
+      .slice(index + 1)
+      .find((number, secondIndex, array) => {
+        const inverse = firstInverse - number;
+        return inverse > 0
+          ? array.slice(secondIndex + 1).includes(inverse)
+          : false;
+      });
+    if (secondEntry) {
+      break;
+    }
+  }
+
+  return firstEntry * secondEntry * (2020 - firstEntry - secondEntry);
+}
+
+export const answer: AnswerFunction = ([input]) => {
+  const expenses = input.split("\n").map(Maths.parseInt);
+
+  return [findTwoExpensesProduct(expenses), findThreeExpensesProduct(expenses)];
 };
