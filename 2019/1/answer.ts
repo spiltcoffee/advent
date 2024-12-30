fea4cfe861316aa74842907b08bc66af47ecb64b
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-30 20:56:26 +1100

    Add 2019 day 1 part 2

--- 2019/1/answer.ts
+++ 2019/1/answer.ts
@@ -1,12 +1,25 @@
 import { AnswerFunction } from "../../answer.ts";
 import { Maths } from "../../common/maths.ts";
 
+function calculateFuel(mass: number): number {
+  return Math.floor(mass / 3) - 2;
+}
+
+function recurseFuel(mass: number): number {
+  const fuel = calculateFuel(mass);
+  if (fuel > 0) {
+    return fuel + recurseFuel(fuel);
+  }
+
+  return 0;
+}
+
 export const answer: AnswerFunction = ([input]) => {
   const masses = input.split("\n").map(Maths.parseInt);
 
-  const fuels = masses.map((mass) => Math.floor(mass / 3) - 2);
+  const simpleFuelTotal = Maths.sum(masses.map(calculateFuel));
 
-  const totalFuel = Maths.sum(fuels);
+  const advancedFuelTotal = Maths.sum(masses.map(recurseFuel));
 
-  return [totalFuel];
+  return [simpleFuelTotal, advancedFuelTotal];
 };
