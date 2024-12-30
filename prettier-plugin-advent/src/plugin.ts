2567dbfb5abee01d9a867bd2c76dda819a0da3c1
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-30 21:49:13 +1100

    Format all inputs and outputs

---
+++ prettier-plugin-advent/src/plugin.ts
@@ -0,0 +1,35 @@
+import { Parser, Printer, SupportLanguage, SupportOption } from "prettier";
+
+export const languages: Partial<SupportLanguage>[] = [
+  {
+    name: "advent-io",
+    parsers: ["advent-io"],
+    extensions: [".txt"],
+    vscodeLanguageIds: ["advent-io"]
+  }
+];
+
+export const parsers: Record<string, Parser<string>> = {
+  "advent-io": {
+    astFormat: "advent-io",
+    parse(text) {
+      return text;
+    },
+    locStart() {
+      return 0;
+    },
+    locEnd(node) {
+      return node.length;
+    }
+  }
+};
+
+export const printers: Record<string, Printer<string>> = {
+  "advent-io": {
+    print(path) {
+      return path.getNode().trim();
+    }
+  }
+};
+
+export const options: Record<string, SupportOption> = {};
