19e3d9680267df80e73ea66e935c3d89b2d43b60
spiltcoffee <spiltcoffee@users.noreply.github.com>
2025-01-02 01:09:19 +1100

    Add 2015 day 8 part 2

--- 2015/8/answer.ts
+++ 2015/8/answer.ts
@@ -1,8 +1,18 @@
 import { AnswerFunction } from "../../answer.ts";
 
+function encode(line: string): string {
+  return `"${line.replaceAll(/(["\\])/g, "\\$1")}"`;
+}
+
 export const answer: AnswerFunction = ([input]) => {
-  const diff = input
-    .split("\n")
-    .reduce((diff, line) => diff + line.length - eval(line).length, 0);
-  return [diff];
+  const lines = input.split("\n");
+  const part1Diff = lines.reduce(
+    (diff, line) => diff + line.length - eval(line).length,
+    0
+  );
+  const part2Diff = lines.reduce(
+    (diff, line) => diff + encode(line).length - line.length,
+    0
+  );
+  return [part1Diff, part2Diff];
 };
